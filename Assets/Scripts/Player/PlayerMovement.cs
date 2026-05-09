ad099b185e04037dca74538288ec2759c0f58900
Yasin Kapaklıkaya <yasinkapaklikaya@192.168.1.33>
2026-05-09 19:25:27 +0300

    feat: add animations for player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,21 +5,51 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
 
     private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer spriteRenderer; 
     private Vector2 moveInput;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
         moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveInput.Normalize(); 
+
+        anim.SetFloat("Speed", moveInput.sqrMagnitude);
+
+        if (moveInput.x != 0)
+        {
+            spriteRenderer.flipX = moveInput.x < 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Attack");
+        }
     }
 
     private void FixedUpdate()
     {
         rb.linearVelocity = moveInput * moveSpeed;
     }
+
+    public void TakeDamage()
+    {
+        anim.SetTrigger("Hit");
+    }
+
+    public void Die()
+    {
+        anim.SetBool("isDead", true);
+        this.enabled = false;
+    }
+
+
 }
